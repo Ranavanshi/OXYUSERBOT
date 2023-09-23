8d7fe022e346b351093b46352b009182863deb7b
Divyanshu Rana <141987294+Ranavanshi@users.noreply.github.com>
2023-09-24 03:11:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ API_ID = int(getenv("API_ID", None))
 API_HASH = getenv("API_HASH", None)
 SESSION1 = getenv("SESSION1", None)
 BOT_TOKEN = getenv("BOT_TOKEN", None)
-OWNER_ID = list(map(int, getenv("OWNER_ID", "5059737154").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "1995154708").split()))
 
 
 #-------------------------------- OPTIONAL -------------------------------------#
@@ -23,19 +23,19 @@ SESSION3 = getenv("SESSION3")
 SESSION4 = getenv("SESSION4")
 SESSION5 = getenv("SESSION5")
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/PRADHAN474/OXYUSERBOT")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Ranavanshi/OXYUSERBOT")
 
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-EXTRA_IMG = getenv("EXTRA_IMG", "https://graph.org/file/b0825ba6490d2aa6a6afd.jpg")
+EXTRA_IMG = getenv("EXTRA_IMG", "http://telegra.ph/file/a06f6181d77a18161063c.jpg")
 
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5059737154 5911205161").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1995154708 ").split()))
 
 for y in OWNER_ID:
     SUDO_USERS.append(y)
